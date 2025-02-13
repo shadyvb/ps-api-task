78bacf77751f11d3abb6d26ec942259e78ffcd4b
Shadi Sharaf <shady@sharaf.me>
2025-02-13 12:37:16 +0000

    Add basic controller methods and API routes.

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\ServiceProvider;
+
 return [
 
     /*
@@ -122,5 +124,12 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
+
+    'providers' => ServiceProvider::defaultProviders()->merge([
+        /*
+         * Application Service Providers...
+         */
+        App\Providers\RouteServiceProvider::class,
+    ])->toArray(),
 
 ];
